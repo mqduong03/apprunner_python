dc7a05b85b681c23510f6214ae8636c8180ac2d3
mqduong03 <mqduong@amazon.com>
2025-10-29 10:58:02 -0400

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ def hello_world(request):
     name = os.environ.get('NAME')
     if name == None or len(name) == 0:
         name = "world"
-    message = "Hello, " + name + "!\n"
+    message = "Hello, " + name + "!\nHave a great day."
     return Response(message)
 
 if __name__ == '__main__':
